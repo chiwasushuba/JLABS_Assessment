c1f151101570207234460b840eefbbfab5363266
whoops <142461198+chiwasushuba@users.noreply.github.com>
2026-01-07 11:57:11 +0800

    Update monitorDisk.sh

--- monitorDisk.sh
+++ monitorDisk.sh
@@ -2,11 +2,10 @@
 
 threshold=80
 
-USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
-
+USAGE=$(df --output=pcent / | tail -1 | tr -dc '0-9')
 
 if [ "$USAGE" -gt "$threshold" ]; then
         echo "Warning: Disk Usage exceeds 80% capacity and is at ${USAGE}%."
 else
-        echo "Disk usage is below 80% threshold at ${USAGE}."
+        echo "Safe: Disk usage is below 80% threshold at ${USAGE}%."
 fi
